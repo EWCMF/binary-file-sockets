32125e40ad2bdba9eb1c21e81773df2a7b4ab856
Tommy <54972963+EWCMF@users.noreply.github.com>
2021-10-14 21:39:01 +0200

    Removed unused functions.

--- BinaryFileSocketsSend/src/main/java/main/Send.java
+++ BinaryFileSocketsSend/src/main/java/main/Send.java
@@ -130,22 +130,4 @@ public class Send {
         }
         return sb.toString();
     }
-
-    private byte[] longToBytes(long l) {
-        byte[] result = new byte[Long.BYTES];
-        for (int i = Long.BYTES - 1; i >= 0; i--) {
-            result[i] = (byte)(l & 0xFF);
-            l >>= Byte.SIZE;
-        }
-        return result;
-    }
-
-    private long bytesToLong(final byte[] b) {
-        long result = 0;
-        for (int i = 0; i < Long.BYTES; i++) {
-            result <<= Byte.SIZE;
-            result |= (b[i] & 0xFF);
-        }
-        return result;
-    }
 }
